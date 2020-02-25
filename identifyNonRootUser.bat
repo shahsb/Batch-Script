c3a5c1787b6a8ddb03fc16d9945a6dd7d25c4ca0
Shakti Shah <shaktishah33@gmail.com>
2020-02-25 18:18:44 +0530

    Update identifyNonRootUser.bat

--- identifyNonRootUser.bat
+++ identifyNonRootUser.bat
@@ -5,5 +5,9 @@ if %errorLevel% == 0 (
 ) else (
     echo Failure: Current permissions inadequate.
     echo NON-ADMIN USER
+    echo creating dir
+    mkdir %temp%\Veritas\NetBackup\logs\user_ops\nbjlogs
+    echo dir created successfully
+    set logFile=%temp%\Veritas\NetBackup\logs\user_ops\nbjlogs\%nbjLogFileName%
 )
 pause
